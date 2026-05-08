aade4ba06e0fc08a0fe0697f3f0a6204c06211b1
João Mourato <joaomouratocn@gmail.com>
2026-05-08 16:11:45 -0300

    First commit

---
+++ src/main/java/br/com/arthivia/EasyInviteApi/EasyInviteApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.arthivia.EasyInviteApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyInviteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyInviteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arthivia/EasyInviteApi/EasyInviteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arthivia.EasyInviteApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyInviteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
